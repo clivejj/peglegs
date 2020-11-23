0a205a074c0b35e879f98871311cac7cb1a33a45
Damian Wasilewicz <damian_wasilewicz@brown.edu>
2020-11-23 13:57:57 -0500

    Skeleton of model and training process

--- assignment.py
+++ assignment.py
@@ -3,6 +3,54 @@ import numpy as np
 from tensorflow.keras import Model
 from preprocessing import get_vec
 
+def train(model, train_inputs, emotion_labels, sentiment_labels):
+    train_inputs = np.array(train_inputs)
+    emotion_labels = np.array(emotion_labels)
+    sentiment_labels = np.array(sentiment_labels)
+
+
+    random_indices = tf.random.shuffle(tf.range(train_inputs.shape[0]))
+    train_inputs = tf.gather(train_inputs, random_indices)
+    emotion_labels = tf.gather(emotion_labels, random_indices)
+    sentiment_labels = tf.gather(sentiment_labels, random_indices)
+
+    for batch_num in range(int(train_inputs.shape[0]/model.batch_size)):
+    	batch_inputs, batch_emotion_labels, batch_sentiment_labels = get_batch(train_inputs, 
+    		emotion_labels, sentiment_labels, batch_num, model.batch_size)
+
+    	with tf.GradientTape() as tape:
+    		emotion_logits, sentiment_logits = model.call(batch_inputs, None)
+    		emotion_batch_loss = model.loss_function(emotion_logits, batch_emotion_labels)
+    		sentiment_batch_loss = model.loss_function(sentiment_logits, batch_sentiment_labels)
+    		batch_loss = emotion_batch_loss + sentiment_batch_loss
+
+    	gradients = tape.gradient(batch_loss, model.trainable_parameters)
+    	model.optimizer.apply_gradients(zip(gradients, model.trainable_parameters))
+
+def test(model, test_inputs, emotion_labels, sentiment_labels):
+
+	test_inputs = np.array(test_inputs)
+	emotion_labels = np.array(emotion_labels)
+	sentiment_labels = np.array(sentiment_labels)
+
+	loss = tf.Variable([0], dtype=tf.float32)
+
+	for batch_num in range(int(train_inputs.shape[0]/model.batch_size)):
+		batch_inputs, batch_emotion_labels, batch_sentiment_labels = get_batch(test_inputs,
+			emotion_labels, sentiment_labels, batch_num, model.batch_size)
+
+			emotion_logits, sentiment_logits = model.call(batch_inputs, None)
+			emotion_batch_loss = model.loss_function(emotion_logits, batch_emotion_labels)
+			sentiment_batch_loss = model.loss_function(sentiment_logits, batch_sentiment_labels)
+
+			loss = loss + batch_loss
+
+	'''
+	Calculate F1 score for emotion and sentiment, probably using tfa.metrics.F1Score
+	and then print each
+
+	Maybe make use of some visualization tool?
+
+	'''
+
 def main():
-	word_2_vec = get_vec()
-	print("Success!")
--- model.py
+++ model.py
@@ -16,8 +16,78 @@ class Model(tf.keras.Model):
 		#Trainable parameters
 		self.biLSTM = tf.keras.layers.Bidirectional(tf.keras.layers.LSTM(units = self.h1, 
 			return_sequences = True, return_state = True))
-		self.Embedding = get_vec()
-		
+		#Get pre-trained embedding layer 
+		self.embedding = get_vec()
+		self.primary_attention_layer = tf.keras.layers.Dense()
+		self.emotion_output_layer = tf.keras.layers.Dense(units = 8, activation = 'sigmoid')
+		self.sentiment_output_layer = tf.keras.layers.Dense(units = 3, activation = 'sigmoid')
+
+
+
+	def call(self, tweets, initial_state):
+
+		#get embedding from word2vec embedding layer
+		embedding = tf.nn.embedding_lookup(self.Embedding, tweets)
+
+		#get outputs from biLSTM
+		whole_seq, final_memory_output, final_carry_output = self.biLSTM(embedding, initial_state = initial_state)
+		'''
+		TODO: Pass in hidden state output of biLSTM layer into primary attention layer for emotion
+		and for sentiment primary attention layers
+
+		Then, pass in the output of each of these, which will be h bar and h cap respectively,
+		into their respective secondary attention layers to get H bar and H cap respective
+
+		Add in dropout!
+		'''	
+
+		h_bar = primary_attention( (final_memory_output,final_carry_output) )
+
+		h_cap = primary_attention( (final_memory_output, final_carry_output) )
+
+		H_bar = secondary_attention(h_bar)
+
+		H_cap = secondary_attention(h_cap)
+
+		emotion_logits = tf.nn.softmax(self.emotion_output_layer(H_bar))
+
+		sentiment_logits = tf.nn.softmax(self.sentiment_output_layer(H_cap))
+
+		return emotion_logits, sentiment_logits
+
+
+
+
+	def primary_attention(self, hidden_state):
+		'''
+		TODO: 
+		for each of the 4 synonyms of a word (retrieved using wn.synset),
+		calculate its attention using tf.math.exp( tf.matmul(
+		tf.matmul(tf.transpose(hidden_state for word), self.primary_attention) + self.primary_bias, synonym )
+
+		Then, calculate m for the word by summing this up, and then create h by concatenating the
+		hidden state for the word and m
+		'''
+		pass
+
+	def secondary_attention(self, primary_attention_output):
+		'''
+		TODO:
+		for each of the words in a sentence,
+		calculate its attention using tf.math.exp(tf.math.tanh(
+		tf.matmul(tf.transpose(hidden_state for word), self.secondary_attention) + self.secondary_bias))
+
+		Then, calculate m for the word by summing this up, and then create h by concatenating the
+		hidden state for the word and m
+		'''
+		pass
+
+
+	def loss_function(self, labels, logits):
+		#Calculate the sum of the loss by comparing the labels with the inputted logits
+		return tf.sum(tf.nn.sigmoid_cross_entropy_with_logits(labels, logits));
+
+
 
 
 
