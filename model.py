f2b2e6dcdd423523b41cc92ed5bea77878de8905
Clive Johnston <clive_johnston@brown.edu>
2020-12-06 11:25:54 -0500

    fix enum

--- model.py
+++ model.py
@@ -73,7 +73,8 @@ class Model(tf.keras.Model):
             0, sentence[0], out, hidden_states, embedding_matrix, synonym_indices
         )
         # construct remaining rows of hs matrix
-        for index, word in enumerate(sentence[1:]):
+        for index, word in range(1, len(sentence)):
+            word = sentence[index]
             h = construct_row(
                 index, word, out, hidden_states, embedding_matrix, synonym_indices
             )
