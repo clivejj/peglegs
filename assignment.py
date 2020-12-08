054208a9808effcc15de44131f4ea4d560bfa72d
Damian Wasilewicz <damian_wasilewicz@brown.edu>
2020-12-07 23:33:01 -0500

    Implementing diff versions of the model

--- assignment.py
+++ assignment.py
@@ -4,153 +4,242 @@ from tensorflow.keras import Model
 from utils import unpickle, get_batch, setup
 from preprocessing import get_vec
 from model import Model
-from sklearn.metrics import f1_score
+from sklearn.metrics import f1_score, recall_score, precision_score
 
 
 def train(
-    model, train_inputs, emotion_labels, sentiment_labels, embeddings, synonym_indices
+	model, train_inputs, emotion_labels, sentiment_labels, embeddings, synonym_indices
 ):
 
-    for batch_num in range(int(len(train_inputs) / model.batch_size)):
+	for batch_num in range(int(len(train_inputs) / model.batch_size)):
 
-        print("Batch Number", batch_num)
-        batch_inputs, batch_emotion_labels, batch_sentiment_labels = get_batch(
-            train_inputs, emotion_labels, sentiment_labels, batch_num, model.batch_size
-        )
+		print("Batch Number", batch_num)
+		batch_inputs, batch_emotion_labels, batch_sentiment_labels = get_batch(
+			train_inputs, emotion_labels, sentiment_labels, batch_num, model.batch_size
+		)
 
-        with tf.GradientTape() as tape:
-            emotion_batch_loss = 0
-            sentiment_batch_loss = 0
-            acc = []
+		if model.type == "full" or model.type == "multi_s":
+			with tf.GradientTape() as tape:
+				emotion_batch_loss = 0
+				sentiment_batch_loss = 0
+				acc = []
 
-            for index, tweet in enumerate(batch_inputs):
-                emotion_logits, sentiment_logit = model.call(
-                    tweet, embeddings, synonym_indices
-                )
-                emotion_batch_loss += model.loss_function(
-                    tf.expand_dims(batch_emotion_labels[index], 0), emotion_logits,
-                )
-                sentiment_batch_loss += model.loss_function(
-                    tf.expand_dims(batch_sentiment_labels[index], 0,), sentiment_logit,
-                )
-                acc += [sentiment_logit]
-            batch_loss = (emotion_batch_loss / 8) + sentiment_batch_loss
+				for index, tweet in enumerate(batch_inputs):
+					emotion_logits, sentiment_logit = model.call(
+						tweet, embeddings, synonym_indices
+					)
+					emotion_batch_loss += model.loss_function(
+						tf.expand_dims(batch_emotion_labels[index], 0), emotion_logits
+					)
+					sentiment_batch_loss += model.loss_function(
+						tf.expand_dims(batch_sentiment_labels[index], 0), sentiment_logit
+					)
+					acc += [sentiment_logit]
+				batch_loss = (emotion_batch_loss / 4) + sentiment_batch_loss
 
-        acc = np.squeeze(
-            tf.cast(tf.math.sigmoid(tf.convert_to_tensor(acc)) > 0.5, np.float32), 2
-        )
-        accuracy = tf.reduce_sum(tf.cast(acc == batch_sentiment_labels, tf.int32))
-        print("Batch Accuracy", accuracy / 64)
-        print("Batch Loss", batch_loss)
-        # batch_loss = emotion_batch_loss + sentiment_batch_loss
+			acc = np.squeeze(
+				tf.cast(tf.math.sigmoid(tf.convert_to_tensor(acc)) > 0.5, np.float32), 2
+			)
+			accuracy = tf.reduce_sum(tf.cast(acc == batch_sentiment_labels, tf.int32))
+			print("Batch Accuracy", accuracy / 64)
+			print("Batch Loss", batch_loss)
+			gradients = tape.gradient(batch_loss, model.trainable_variables)
+			model.optimizer.apply_gradients(zip(gradients, model.trainable_variables))
 
-        gradients = tape.gradient(batch_loss, model.trainable_variables)
-        model.optimizer.apply_gradients(zip(gradients, model.trainable_variables))
+		elif model.type == "sentiment_only_p_and_s" or model.type == "sentiment_only_s":
+			with tf.GradientTape() as tape:
+				sentiment_batch_loss = 0
+				acc = []
+
+				for index, tweet in enumerate(batch_inputs):
+					sentiment_logit = model.call(
+						tweet, embeddings, synonym_indices
+					)
+					sentiment_batch_loss += model.loss_function(
+						tf.expand_dims(batch_sentiment_labels[index], 0), sentiment_logit
+					)
+					acc += [sentiment_logit]
+				batch_loss = sentiment_batch_loss
+			acc = np.squeeze(
+				tf.cast(tf.math.sigmoid(tf.convert_to_tensor(acc)) > 0.5, np.float32), 2
+			)  
+			accuracy = tf.reduce_sum(tf.cast(acc == batch_sentiment_labels, tf.int32))
+			print("Batch Accuracy", accuracy / 64)
+			print("Batch Loss", batch_loss)
+			gradients = tape.gradient(batch_loss, model.trainable_variables)
+			model.optimizer.apply_gradients(zip(gradients, model.trainable_variables))
+
+		elif model.type == "emotion_only_p_and_s" or model.type == "emotion_only_s" :
+			with tf.GradientTape() as tape:
+				emotion_batch_loss = 0
+				acc = []
+
+				for index, tweet in enumerate(batch_inputs):
+					emotion_logits = model.call(
+						tweet, embeddings, synonym_indices
+					)
+					emotion_batch_loss += model.loss_function(
+						tf.expand_dims(batch_emotion_labels[index], 0), emotion_logits
+					)
+				batch_loss = emotion_batch_loss / 4 
+			# acc = np.squeeze(
+			# 	tf.cast(tf.math.sigmoid(tf.convert_to_tensor(acc)) > 0.5, np.float32), 2
+			# )  
+			print("Batch Loss", batch_loss)
+			gradients = tape.gradient(batch_loss, model.trainable_variables)
+			model.optimizer.apply_gradients(zip(gradients, model.trainable_variables))
+		else: 
+			print("Invalid Model Type")
+			return
+
 
 
 def test(
-    model, test_inputs, emotion_labels, sentiment_labels, embeddings, synonym_indices
+	model, test_inputs, emotion_labels, sentiment_labels, embeddings, synonym_indices
 ):
-    emotionF1 = 0
-    emotionRecall = 0
-    sentimentF1 = 0
-    sentimentPrecision = 0
+	emotionF1 = 0
+	emotionRecall = 0
+	sentimentAcc = 0
 
-    for index, tweet in enumerate(test_inputs):
-        print("Tweet", index)
-        emotion_logits, sentiment_logits = model.call(
-            tweet, embeddings, synonym_indices,
-        )
+	for index, tweet in enumerate(test_inputs):
+		print("Tweet", index)
+		if model.type == "full" or model.type == "multi_s":
+			emotion_logits, sentiment_logits = model.call(
+				tweet, embeddings, synonym_indices
+			)
 
-        emotion_predictions = tf.squeeze(
-            tf.cast(tf.math.sigmoid(emotion_logits) > 0.5, tf.float32), 0
-        )
-        sentiment_predictions = tf.cast(
-            tf.math.sigmoid(sentiment_logits) > 0.5, tf.float32
-        )
-        # print(emotion_labels[index, :])
-        # print(emotion_predictions)
-        emotion_f1_batch = f1_score(emotion_labels[index, :], emotion_predictions)
-        # print(sentiment_labels[index, :])
-        # print(sentiment_predictions)
-        sentiment_f1_batch = tf.cast(
-            sentiment_labels[index, :] == sentiment_predictions, tf.float32
-        )
-        emotionF1 += emotion_f1_batch
-        sentimentF1 += sentiment_f1_batch
-        if index % 100 == 0:
-            print(sentimentF1 / index)
+			emotion_predictions = tf.squeeze(
+				tf.cast(tf.math.sigmoid(emotion_logits) > 0.5, tf.float32), 0
+			)
 
-    average_emotion_F1 = emotionF1 / len(test_inputs)
-    print("Average Emotion F1", average_emotion_F1)
-    # average_emotion_Recall = emotionRecall / len(test_inputs)
-    # print("Average Emotion Recall", average_emotion_Recall)
-    average_sentiment_F1 = sentimentF1 / len(test_inputs)
-    print("Average Sentiment F1", average_sentiment_F1)
-    # average_sentiment_Precision = sentimentPrecision / len(test_inputs)
-    # print("Average Sentiment Precision", average_sentiment_Precision)
+			emotion_f1_batch = f1_score(emotion_labels[index, :], emotion_predictions)
+			emotion_recall_batch = recall_score(emotion_labels[index, :], emotion_predictions)
+			emotionF1 += emotion_f1_batch
+			emotionRecall += emotion_recall_batch
+
+			sentiment_predictions = tf.cast(
+				tf.math.sigmoid(sentiment_logits) > 0.5, tf.float32
+			)
+			sentiment_acc_batch = tf.cast(
+				sentiment_labels[index, :] == sentiment_predictions, tf.float32
+			)
+			sentimentAcc += sentiment_acc_batch
+
+		elif model.type == "sentiment_only_p_and_s" or model.type == "sentiment_only_s":
+			sentiment_logits = model.call(
+				tweet, embeddings, synonym_indices
+			)
+			sentiment_predictions = tf.cast(
+				tf.math.sigmoid(sentiment_logits) > 0.5, tf.float32
+			)
+			sentiment_acc_batch = tf.cast(
+				sentiment_labels[index, :] == sentiment_predictions, tf.float32
+			)
+			sentimentAcc += sentiment_acc_batch
+
+
+		elif model.type == "emotion_only_p_and_s" or model.type == "emotion_only_s":
+			emotion_logits = model.call(
+				tweet, embeddings, synonym_indices
+			)
+			emotion_predictions = tf.squeeze(
+				tf.cast(tf.math.sigmoid(emotion_logits) > 0.5, tf.float32), 0
+			)
+
+			emotion_f1_batch = f1_score(emotion_labels[index, :], emotion_predictions)
+			emotion_recall_batch = recall_score(emotion_labels[index, :], emotion_predictions)
+			emotionF1 += emotion_f1_batch
+			emotionRecall += emotion_recall_batch
+		else:
+			print("Invalid Model Type")
+			return
+
+		# if index % 100 == 0:
+		#     print(sentimentAcc / index)
+	if model.type == "full" or model.type == "multi_s":
+		average_emotion_F1 = emotionF1 / len(test_inputs)
+		print("Average Emotion F1", average_emotion_F1)
+		average_emotion_Recall = emotionRecall / len(test_inputs)
+		print("Average Emotion Recall", average_emotion_Recall)
+		average_sentiment_acc = sentimentAcc / len(test_inputs)
+		print("Average Sentiment Acc", average_sentiment_acc)
+
+	elif model.type == "sentiment_only_p_and_s" or model.type == "sentiment_only_s":
+		average_sentiment_acc = sentimentAcc / len(test_inputs)
+		print("Average Sentiment Acc", average_sentiment_acc)
+
+	elif model.type == "emotion_only_p_and_s" or model.type == "emotion_only_s":
+		average_emotion_F1 = emotionF1 / len(test_inputs)
+		print("Average Emotion F1", average_emotion_F1)
+		average_emotion_Recall = emotionRecall / len(test_inputs)
+		print("Average Emotion Recall", average_emotion_Recall)
+
+	else: 
+		print("Invalid Model Type")
+		return
+  
 
 
 def main():
-    # Unpickles data from file, stores it as dictionary of length 4
-    setup(isTraining=True, overwrite=False)
-    print("done processing training data")
-    data = unpickle("data/training.pickle")
-    # A dictionary that keys every word in our vocabulary to an index
-    train_vocab = data[0]
-    # A list of the tweets that we will be training on (2914 tweets)
-    train_sentences = data[1]
-    """for i in range(len(train_sentences)):
-        train_sentences[i] = tf.convert_to_tensor(train_sentences[i], tf.int32)"""
-    # print("Sentences", len(sentences))
-    # An embedding matrix that maps each word to a 300 Dimensional Embedding
-    train_embeddings = tf.convert_to_tensor(data[2], tf.float32)
-    # A dictionary that maps the index of a word to a list containing the indices of its 4 synonyms
-    train_synonym_indices = tf.convert_to_tensor(data[3], tf.int32)
+	# Unpickles data from file, stores it as dictionary of length 4
+	setup(isTraining=True, overwrite=False)
+	print("done processing training data")
+	data = unpickle("data/training.pickle")
+	# A dictionary that keys every word in our vocabulary to an index
+	train_vocab = data[0]
+	# A list of the tweets that we will be training on (2914 tweets)
+	train_sentences = data[1]
+	"""for i in range(len(train_sentences)):
+		train_sentences[i] = tf.convert_to_tensor(train_sentences[i], tf.int32)"""
+	# print("Sentences", len(sentences))
+	# An embedding matrix that maps each word to a 300 Dimensional Embedding
+	train_embeddings = tf.convert_to_tensor(data[2], tf.float32)
+	# A dictionary that maps the index of a word to a list containing the indices of its 4 synonyms
+	train_synonym_indices = tf.convert_to_tensor(data[3], tf.int32)
 
-    # A list of sentiment labels corresponding to tweets; labels can be -1 (negative), 0 (objective), or (1) positive
-    # (2914, 1)
-    train_sentiment_labels = tf.convert_to_tensor(data[4], tf.float32)
-    # A list of emotion labels corresponding to tweets; each label has 8 slots, where a 1 in that position corresponds to that
-    # emotion being labelled. So, each tweet can be associated to several different emotions
-    # Shape (2914, 8)
-    train_emotion_labels = tf.convert_to_tensor(data[5], tf.float32)
-    data = None
+	# A list of sentiment labels corresponding to tweets; labels can be -1 (negative), 0 (objective), or (1) positive
+	# (2914, 1)
+	train_sentiment_labels = tf.convert_to_tensor(data[4], tf.float32)
+	# A list of emotion labels corresponding to tweets; each label has 8 slots, where a 1 in that position corresponds to that
+	# emotion being labelled. So, each tweet can be associated to several different emotions
+	# Shape (2914, 8)
+	train_emotion_labels = tf.convert_to_tensor(data[5], tf.float32)
+	data = None
 
-    model = Model()
+	model = Model("emotion_only_s")
 
-    train(
-        model,
-        train_sentences,
-        train_emotion_labels,
-        train_sentiment_labels,
-        train_embeddings,
-        train_synonym_indices,
-    )
+	train(
+		model,
+		train_sentences,
+		train_emotion_labels,
+		train_sentiment_labels,
+		train_embeddings,
+		train_synonym_indices,
+	)
 
-    setup(isTraining=False, overwrite=False)
-    print("done processing testing data")
-    data = unpickle("data/testing.pickle")
+	setup(isTraining=False, overwrite=False)
+	print("done processing testing data")
+	data = unpickle("data/testing.pickle")
 
-    test_vocab = data[0]
-    test_sentences = data[1]
-    """for i in range(len(train_sentences)):
-        train_sentences[i] = tf.convert_to_tensor(train_sentences[i], tf.int32)"""
-    test_embeddings = tf.convert_to_tensor(data[2], tf.float32)
-    test_synonym_indices = tf.convert_to_tensor(data[3], tf.int32)
-    test_sentiment_labels = tf.convert_to_tensor(data[4], tf.float32)
-    test_emotion_labels = tf.convert_to_tensor(data[5], tf.float32)
-    data = None
+	test_vocab = data[0]
+	test_sentences = data[1]
+	"""for i in range(len(train_sentences)):
+		train_sentences[i] = tf.convert_to_tensor(train_sentences[i], tf.int32)"""
+	test_embeddings = tf.convert_to_tensor(data[2], tf.float32)
+	test_synonym_indices = tf.convert_to_tensor(data[3], tf.int32)
+	test_sentiment_labels = tf.convert_to_tensor(data[4], tf.float32)
+	test_emotion_labels = tf.convert_to_tensor(data[5], tf.float32)
+	data = None
 
-    test(
-        model,
-        test_sentences,
-        test_emotion_labels,
-        test_sentiment_labels,
-        test_embeddings,
-        test_synonym_indices,
-    )
+	test(
+		model,
+		test_sentences,
+		test_emotion_labels,
+		test_sentiment_labels,
+		test_embeddings,
+		test_synonym_indices,
+	)
 
 
 if __name__ == "__main__":
-    main()
+	main()
--- model.py
+++ model.py
@@ -4,23 +4,31 @@ from utils import construct_row
 
 
 class Model(tf.keras.Model):
-    def __init__(self):
+    def __init__(self, type):
         super(Model, self).__init__()
 
-        # HyperParamaters
+        # HyperParameters
         self.optimizer = tf.keras.optimizers.Adam(learning_rate=0.01)
         self.embedding_size = 300
         self.h1 = 300
         self.batch_size = 64
+        self.type = type
         # Trainable parameters
 
         self.biLSTM = tf.keras.layers.Bidirectional(
             tf.keras.layers.LSTM(units=int(self.h1 / 2), return_sequences=True,)
         )
-        self.primary_attention_dense_layer = tf.keras.layers.Dense(self.h1)
-        self.secondary_attention_dense_layer = tf.keras.layers.Dense(
+
+        self.primary_attention_emotion_dense_layer = tf.keras.layers.Dense(self.h1)
+        self.primary_attention_sentiment_dense_layer = tf.keras.layers.Dense(self.h1)
+
+        self.secondary_attention_emotion_dense_layer = tf.keras.layers.Dense(
             1, activation="tanh"
         )
+        self.secondary_attention_sentiment_dense_layer = tf.keras.layers.Dense(
+            1, activation="tanh"
+        )
+
         self.emotion_output_layer = tf.keras.layers.Dense(units=8)
         self.sentiment_output_layer = tf.keras.layers.Dense(units=1)
 
@@ -31,30 +39,97 @@ class Model(tf.keras.Model):
         hidden_states = tf.squeeze(self.biLSTM(tf.expand_dims(embeddings, 0)))
         # print(hidden_states)
         # print("Hidden states shape", np.shape(hidden_states))
-        h_hats = self.primary_attention(
-            sentence, hidden_states, embedding_matrix, synonym_indices
-        )
-        # print("h_hats Shape post call", np.shape(h_hats))
+        if(self.type == "full"):
 
-        h_bars = h_hats
+            h_hats = self.primary_attention_sentiment(
+                sentence, hidden_states, embedding_matrix, synonym_indices
+            )
 
-        H_HAT = tf.expand_dims(self.secondary_attention(h_hats), 0)
+            h_bars = self.primary_attention_emotion(
+                sentence, hidden_states, embedding_matrix, synonym_indices
+            )
 
-        H_BAR = H_HAT
-        # print("H_HAT Shape", np.shape(H_HAT))
+            H_HAT = tf.expand_dims(self.secondary_attention_sentiment(h_hats), 0)
 
-        emotion_logits = self.emotion_output_layer(H_BAR)
-        # print("Emotion Logits Shape", np.shape(emotion_logits))
-        # emotion_logits = tf.where(emotion_logits > .5, 1.0, 0.0)
-        # print("Emotion Logits", emotion_logits)
+            H_BAR = tf.expand_dims(self.secondary_attention_emotion(h_bars), 0)
 
-        sentiment_logits = self.sentiment_output_layer(H_HAT)
-        # sentiment_logits = tf.where(sentiment_logits > .5, 1.0, -1.0)
+            sentiment_logits = self.sentiment_output_layer(H_HAT)
 
-        # return emotion_logits, sentiment_logit
-        return emotion_logits, sentiment_logits
+            emotion_logits = self.emotion_output_layer(H_BAR)
 
-    def primary_attention(
+            print("Returning Full Call")
+            return emotion_logits, sentiment_logits
+
+
+        elif(self.type == "multi_s"):
+            
+            H_HAT = tf.expand_dims(self.secondary_attention_sentiment(hidden_states), 0)
+        
+            H_BAR = tf.expand_dims(self.secondary_attention_emotion(hidden_states), 0)
+
+            sentiment_logits = self.sentiment_output_layer(H_HAT)
+
+            emotion_logits = self.emotion_output_layer(H_BAR)
+        
+            print("Returning Multi_S Call")
+            return emotion_logits, sentiment_logits
+       
+        elif(self.type == "sentiment_only_p_and_s"):
+
+            h_hats = self.primary_attention_sentiment(
+                sentence, hidden_states, embedding_matrix, synonym_indices
+            )
+
+            H_HAT = tf.expand_dims(self.secondary_attention_sentiment(h_hats), 0)
+
+            sentiment_logits = self.sentiment_output_layer(H_HAT)
+
+            print("Returning Sentiment_Only_P_and_S")
+            return sentiment_logits
+
+        elif(self.type == "sentiment_only_s"):
+
+            H_HAT = tf.expand_dims(self.secondary_attention_sentiment(hidden_states), 0)
+
+            sentiment_logits = self.sentiment_output_layer(H_HAT)
+
+            print("Returning Sentiment_Only_S")
+            return sentiment_logits
+
+        elif(self.type == "emotion_only_p_and_s"):
+
+            h_bars = self.primary_attention_emotion(
+                sentence, hidden_states, embedding_matrix, synonym_indices
+            )
+
+            H_BAR = tf.expand_dims(self.secondary_attention_emotion(h_bars), 0)
+
+            emotion_logits = self.emotion_output_layer(H_BAR)
+
+            print("Returning Emotion Only P and S")
+            return emotion_logits
+
+        elif(self.type == "emotion_only_s"):
+
+            H_BAR = tf.expand_dims(self.secondary_attention_emotion(hidden_states), 0)
+
+            emotion_logits = self.emotion_output_layer(H_BAR)
+
+            print("Returning Emotion Only S")
+            return emotion_logits
+
+        else:
+            print("Invalid Model Type")
+            return
+
+
+
+
+
+
+
+
+    def primary_attention_emotion(
         self, sentence, hidden_states, embedding_matrix, synonym_indices
     ):
         """
@@ -66,23 +141,43 @@ class Model(tf.keras.Model):
 		Then, calculate m for the word by summing this up, and then create h by concatenating the
 		hidden state for the word and m
 		"""
-        out = self.primary_attention_dense_layer(hidden_states)
+        out = self.primary_attention_emotion_dense_layer(hidden_states)
 
         # construct first row of hs matrix
-        hs = construct_row(
+        h_bars = construct_row(
             0, sentence[0], out, hidden_states, embedding_matrix, synonym_indices
         )
         # construct remaining rows of hs matrix
         for index in range(1, len(sentence)):
             word = sentence[index]
-            h = construct_row(
+            h_bar = construct_row(
                 index, word, out, hidden_states, embedding_matrix, synonym_indices
             )
-            hs = tf.concat([hs, h], 0)
+            h_bars = tf.concat([h_bars, h_bar], 0)
 
-        return hs
+        return h_bars
 
-    def secondary_attention(self, h_hats):
+    def primary_attention_sentiment(
+        self, sentence, hidden_states, embedding_matrix, synonym_indices
+    ):
+
+        out = self.primary_attention_sentiment_dense_layer(hidden_states)
+
+        h_hats = construct_row(
+            0, sentence[0], out, hidden_states, embedding_matrix, synonym_indices
+        )
+
+        for index in range(1, len(sentence)):
+            word = sentence[index]
+            h_hat = construct_row(
+                index, word, out, hidden_states, embedding_matrix, synonym_indices
+            )
+            h_hats = tf.concat([h_hats, h_hat], 0)
+
+        return h_hats
+
+
+    def secondary_attention_emotion(self, h_bars):
         """
 		TODO:
 		for each of the words in a sentence,
@@ -92,7 +187,13 @@ class Model(tf.keras.Model):
 		Then, calculate m for the word by summing this up, and then create h by concatenating the
 		hidden state for the word and m
 		"""
-        coefficients = tf.math.exp(self.secondary_attention_dense_layer(h_hats))
+        coefficients = tf.math.exp(self.secondary_attention_emotion_dense_layer(h_bars))
+        return tf.reduce_sum(coefficients * h_bars, 0)
+
+
+    def secondary_attention_sentiment(self, h_hats):
+
+        coefficients = tf.math.exp(self.secondary_attention_sentiment_dense_layer(h_hats))
         return tf.reduce_sum(coefficients * h_hats, 0)
 
     def loss_function(self, labels, logits):
